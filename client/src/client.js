eb4556dda20da83f965aeea5d5d6a33e92cfd0be
Cjeveritt19 <callumeveritt11@gmail.com>
2025-09-04 15:46:27 +0100

    started styling and adding app.get and app.post to my client and server js

--- client/src/client.js
+++ client/src/client.js
@@ -1,1 +1,15 @@
-document.body.style.backgroundcolor = "pink";
+const guestFrom = document.getElementById("guest-form");
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(guestForm);
+    const formValues = Object.fromEntries(formData)
+    console.log(formValues)
+    fetch ("", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({formValues}),
+    });
+}
--- server/server.js
+++ server/server.js
@@ -15,3 +15,12 @@ app.listen(PORT, function() {
 app.get("/", function(req, res) {
     res.json({message: "Welcome to the server. GET comfy" });
 });
+
+
+app.post("/add-GuestBook", (req, res) => {
+    const newGuestBook = req.body.formValues
+    const query = db.query(
+        `inster into GuestBook (name, email, reason, number) values ($1, $2, $3, $4)`,
+        [newGuestBook.name, newGuestBook.email, newGuestBook.reason, newGuestBook.number]);
+        res.json("Data sent", query);
+});
