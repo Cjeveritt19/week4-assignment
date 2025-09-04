18a8393075d56e61356824df6f60602b9b75059c
Cjeveritt19 <callumeveritt11@gmail.com>
2025-09-04 16:11:38 +0100

    added a app.get to the server.js to ensure its working

--- server/server.js
+++ server/server.js
@@ -16,11 +16,16 @@ app.get("/", function(req, res) {
     res.json({message: "Welcome to the server. GET comfy" });
 });
 
+app.get("/Guestbook", async (_, res) => {
+    const query = await  db.query(`select * from GuestBook`)
+    console.log(query);
+    res.json(query.rows);
+});
 
 app.post("/add-GuestBook", (req, res) => {
     const newGuestBook = req.body.formValues;
     const query = db.query(
         `insert into GuestBook (name, email, reason, number) values ($1, $2, $3, $4)`,
         [newGuestBook.name, newGuestBook.email, newGuestBook.reason, newGuestBook.number]);
-        res.json("Data sent", query);
+    res.json("Data sent", query);
 });
